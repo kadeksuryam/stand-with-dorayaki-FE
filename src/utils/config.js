a112554ddb7c70ed89e50083febfa0f7d08643ed
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-27 21:41:15 +0700

    edit api endpoint

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://api-stand-with-dorayaki.eastus.cloudapp.azure.com:5000/api/v1'
+const API_BASE_URL = 'http://stand-with-dorayaki.eastus.cloudapp.azure.com/api/v1'
 
 export { API_BASE_URL }
