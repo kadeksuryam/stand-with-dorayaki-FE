f9de23a065e56eb67558e88957b99b9745dee2a3
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-27 21:39:51 +0700

    implement some Toko Page func

--- src/components/TokoStok.jsx
+++ src/components/TokoStok.jsx
@@ -9,52 +9,96 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from "@material-ui/core/TextField";
 import EditIcon from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
-
+import SaveIcon from '@material-ui/icons/Save';
+import { green } from '@material-ui/core/colors';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Fab from '@material-ui/core/Fab';
+import { API_BASE_URL } from '../utils/config'
+import axios from 'axios';
 const useStyles = makeStyles({
     root: {
-      "maxWidth": "16rem",
+        width: "25vh"
     },
     media: {
-      "height" : "20vh"
+        height: 0,
+        paddingTop: '100%'
     },
+    fabProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: -6,
+        left: -6,
+        zIndex: 1,
+    }
 });
 
-const TokoStok = ({dataTokoStok}) => {
+const TokoStok = ({dataTokoStok, notif, setNotif, syncDataStokToko}) => {
     const classes = useStyles()
-    const [stok, setStok] = useState(0)
+    const [stokField, setStokField] = useState(false)
+    const [editOps, setEditOps] = useState(false)
+    const [stokFieldVal, setStokFielVal] = useState(dataTokoStok.stok)
+
+    const handleEditStok = async () => {
+        if(!stokField){
+            setStokField(true)
+        }
+        else{
+            try{
+                setEditOps(true)
+                const stokEditURL = API_BASE_URL + `/stok-dorayakis/${dataTokoStok._id}` 
+                const tes = await axios.put(stokEditURL, {stok: stokFieldVal})
+                setEditOps(false)
+                setStokField(false)
+                setNotif({...notif, open: true, type: "success", msg: "stok dorayaki berhasil diupdate"})
+                await syncDataStokToko
+            } catch(err){
+                let errMsg = err.message.toString()
+                if(err.response)
+                    if(err.response.data.error)
+                         errMsg = err.response.data.error
+                setNotif({...notif, open: true, type: "error", msg: errMsg})
+            }
+        }
+    }
 
     return(
         <Card className={classes.root}>
-            <CardMedia
-                className={classes.media}
-                image={dataTokoStok.gambar}
-                title={dataTokoStok.nama}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Dorayaki {dataTokoStok.rasa}
-                </Typography>
-                <Typography gutterBottom variant="h6" component="h2">
-                    {dataTokoStok.deskripsi}
-                </Typography>
-            </CardContent>
-            <div style={{display:"flex"}}>
+            <div style={{height: "33vh"}}>
+                <CardMedia
+                    className={classes.media}
+                    image={API_BASE_URL + dataTokoStok.dorayaki.gambar}
+                    title={dataTokoStok.dorayaki.nama}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h6" component="h2">
+                        Dorayaki {dataTokoStok.dorayaki.rasa}
+                    </Typography>
+                </CardContent>
+            </div>
+            <div style={{display:"flex", justifyContent: "space-between"}}>
                 <TextField
                     id="filled-number"
                     label="Stok Dorayaki"
                     type="number"
                     InputProps={{
-                        disabled: true,
-                        defaultValue: 0,
+                        disabled: !stokField,
+                        value: stokFieldVal,
                     }}
                     size="small"
                     variant="outlined"
-                    style={{maxWidth: "10rem", margin: "1rem"}}
+                    style={{maxWidth: "10rem", marginLeft: "1rem"}}
+                    onChange={(e) => setStokFielVal(e.target.value)}
                 />
-                <IconButton color="primary" aria-label="edit stok" component="span">
-                    <EditIcon />
-                </IconButton>
+                <Fab
+                    style={{marginRight: "1rem", marginBottom: "1rem"}}
+                    size="small"
+                    aria-label="save"
+                    color="primary"
+                    onClick={handleEditStok}
+                    >
+                    {stokField ? <SaveIcon /> : <EditIcon />}
+                    {editOps && <CircularProgress size={50} className={classes.fabProgress} />}
+                </Fab>
             </div>
         </Card>
     )
--- src/pages/TokoStokPage.jsx
+++ src/pages/TokoStokPage.jsx
@@ -3,47 +3,70 @@ import {useParams} from 'react-router-dom'
 import Grow from '@material-ui/core/Grow'
 import Paper from '@material-ui/core/Paper'
 import TokoStok from '../components/TokoStok'
-    
+import { API_BASE_URL } from '../utils/config'
+import axios from 'axios'
+
 const stylesPar = {
     "display": "flex",
     "justifyContent": "flex-start",
     "flexWrap" : "wrap"
 }
 
-const TokoStokPage = () => {
+const TokoStokPage = ({notif, setNotif}) => {
     const [tokoStoks, setTokoStoks] = useState([])
-    const { idToko } = useParams()
+    const { id } = useParams()
 
     useEffect(() => {
-        //API CALL, sesuai Id Toko
-        const newTokoStoks = [
-            {
-                rasa : "manis",
-                deskripsi : "dorayaki coklat",
-                gambar : "https://upload.wikimedia.org/wikipedia/commons/7/7f/Dorayaki_001.jpg",
-                stok : 5
+        const getStok = async () => {
+            try{
+                const stokURL = API_BASE_URL + `/stok-dorayakis/?idToko=${id}`
+                const newTokoStoks = (await axios.get(stokURL)).data
+
+                setTokoStoks(newTokoStoks)
+            } catch(err){
+                let errMsg = err.message.toString()
+                if(err.response)
+                    if(err.response.data.error)
+                         errMsg = err.response.data.error
+                setNotif({...notif, open: true, type: "error", msg: errMsg})
             }
-        ]
-        
-        setTokoStoks(newTokoStoks)
+        }
+        getStok()
     }, [])
     
+    const syncDataStokToko = async () => {
+        try{
+            const stokURL = API_BASE_URL + `/stok-dorayakis/?idToko=${id}`
+            const newTokoStoks = (await axios.get(stokURL)).data
+
+            setTokoStoks(newTokoStoks)
+        } catch(err){
+            let errMsg = err.message.toString()
+            if(err.response)
+                if(err.response.data.error)
+                     errMsg = err.response.data.error
+            setNotif({...notif, open: true, type: "error", msg: errMsg})
+        }
+    }
+
     return (
-        <>
+        <div style={{minHeight: "100vh"}}>
             <div style={stylesPar}>
                 {
                     tokoStoks.map((tokoStok, idx) => (
                         <div key={idx}>
                             <Grow in={true} key={idx}>
                                 <Paper  elevation={10} key={idx} style={{"margin" : "1rem"}}>
-                                    <TokoStok key={idx} dataTokoStok={tokoStok}/>
+                                    <TokoStok key={idx} dataTokoStok={tokoStok}
+                                        notif={notif} setNotif={setNotif}
+                                        syncDataStokToko={syncDataStokToko}/>
                                 </Paper>
                             </Grow>
                         </div>
                     ))
                 }
             </div>
-        </>      
+        </div>      
     )
 }
 
