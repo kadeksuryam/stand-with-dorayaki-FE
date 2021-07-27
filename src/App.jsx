a1158abc20f81e6a55da807a8707440771ae0f92
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-27 21:40:53 +0700

    add dorayaki path and preserve theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import Navbarc from './components/Navbar'
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { Paper, Button } from "@material-ui/core"
 import {
   orange,
@@ -15,6 +15,7 @@ import TokoStokPage from './pages/TokoStokPage'
 import { API_BASE_URL } from './utils/config'
 import axios from 'axios'
 import Notification from './components/Notification'
+import DorayakiPage from './pages/DorayakiPage';
 
 function App() {
   const [darkState, setDarkState] = useState(false);
@@ -33,21 +34,34 @@ function App() {
     }
   });
   const handleThemeChange = () => {
+    if(darkState)
+      localStorage.setItem("currTheme", "light")
+    else localStorage.setItem("currTheme", "dark")
     setDarkState(!darkState);
   };
 
   const [dataTokos, setDataTokos] = useState([])
+  const [dataDorayakis, setDataDorayakis] = useState([])
 
   useEffect(() => {
     const getDataToko = async () => {
       try{
         const resDataTokos = (await axios.get(API_BASE_URL + '/toko-dorayakis')).data
+        const resDataDorayakis = (await axios.get(API_BASE_URL + '/dorayakis')).data
+        
         setDataTokos(resDataTokos)
+        setDataDorayakis(resDataDorayakis)
       } catch(err){
-        console.log(err.message)
+        let errMsg = err.message.toString()
+        if(err.response)
+            if(err.response.data.error)
+                 errMsg = err.response.data.error
+        setNotif({...notif, open: true, type: "error", msg: errMsg})
       }
     }
     getDataToko()
+    if(localStorage.getItem("currTheme") === "dark") setDarkState(true)
+    else setDarkState(false)
   }, [])
 
   const syncDataTokos = async () => {
@@ -55,7 +69,25 @@ function App() {
       const resDataTokos = (await axios.get(API_BASE_URL + '/toko-dorayakis')).data
       setDataTokos(resDataTokos)
     } catch(err){
-      console.log(err.message)
+      let errMsg = err.message.toString()
+      if(err.response)
+          if(err.response.data.error)
+               errMsg = err.response.data.error
+      setNotif({...notif, open: true, type: "error", msg: errMsg})
+    }
+  }
+  
+  const syncDataDorayakis = async () => {
+    try{
+      const resDataDorayakis = (await axios.get(API_BASE_URL + '/dorayakis')).data
+      setDataDorayakis(resDataDorayakis)
+      
+    } catch(err){
+      let errMsg = err.message.toString()
+      if(err.response)
+          if(err.response.data.error)
+               errMsg = err.response.data.error
+      setNotif({...notif, open: true, type: "error", msg: errMsg})
     }
   }
 
@@ -68,12 +100,21 @@ function App() {
           <Navbarc darkState={darkState} handleThemeChange={handleThemeChange}/>
           <Paper style={{height: "auto"}}>
             <Switch>
+              <Route path="/dorayaki">
+                <DorayakiPage dataDorayakis={dataDorayakis} syncDataDorayakis={syncDataDorayakis} 
+                  notif={notif} setNotif={setNotif}
+                  />
+              </Route>
+              <Route path="/toko-dorayaki/stok/:id">
+                <TokoStokPage notif={notif} setNotif={setNotif}/>
+              </Route>
               <Route path="/toko-dorayaki">
                 <TokoPage dataTokos={dataTokos} syncDataTokos={syncDataTokos} 
-                  notif={notif} setNotif={setNotif}/>
+                  notif={notif} setNotif={setNotif}
+                  />
               </Route>
-              <Route path="/toko-dorayaki-stok">
-                <TokoStokPage/>
+              <Route path="/">
+                <Redirect to="/toko-dorayaki"/>
               </Route>
             </Switch>
           </Paper>
